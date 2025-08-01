c54b0d511a095a47c49ad09c2a151fdc503907ad
Abdirizak Bashir <41330628+abashir236@users.noreply.github.com>
2025-08-01 12:28:02 -0400

    Update file_upload.py

--- vulns/file_upload/file_upload.py
+++ vulns/file_upload/file_upload.py
@@ -28,7 +28,7 @@ def file_upload_api(request, app):
 
     public_upload_file_path = os.path.join(app.config['PUBLIC_UPLOAD_FOLDER'], file_name)
     
-    os.system(f'mv {saved_file_path} {public_upload_file_path}')
+    subprocess.run(["mv", saved_file_path, public_upload_file_path], check=True)
 
     return render_template('file_upload.html', file_url=f'{get_uploads_folder_url()}/{file_name}')
 
@@ -46,7 +46,7 @@ def _save_temp_file(file, app):
     resized_image_path = f'{temp_upload_file_path}.min.png'
     # https://imagemagick.org/script/convert.php
     command = f'convert "{temp_upload_file_path}" -resize 50% "{resized_image_path}"'
-    os.system(command)
+    subprocess.run(["convert", temp_upload_file_path, "-resize", "50%", resized_image_path], check=True)
 
     return {
         'saved_path': resized_image_path
